6be5ce597233eb871cc64e24afcd8f9c5eb75506
kencal <kenadjei20@gmail.com>
2023-04-23 11:10:05 +0000

    update on index ,css  and main

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
-document.getElementsByTagName("nav")[0].addEventListener("mouseover", function () {
-  document.getElementsByTagName("nav")[0].style.backgroundColor = "white";
-  document.getElementById("navHeading").style.color = "black";
-  document.getElementsByTagName("ul")[0].style.color = "black";
+document;
+//   .getElementsByTagName("nav")[0]
+//   .addEventListener("mouseover", function () {
+//     document.getElementsByTagName("nav")[0].style.backgroundColor = "white";
+//     document.getElementById("navHeading").style.color = "black";
+//     document.getElementsByTagName("ul")[0].style.color = "black";
+//     document.getElementsByTagName("select")[0].style.backgroundColor = "grey";
+//     document.getElementsByTagName("select")[0].style.color = "white";
+//   });
 
-});
+// document
+//   .getElementsByTagName("nav")[0]
+//   .addEventListener("mouseout", function () {
+//     document.getElementsByTagName("nav")[0].style.backgroundColor = null;
+//     document.getElementById("navHeading").style.color = null;
+//     document.getElementsByTagName("ul")[0].style.color = null;
+//     document.getElementsByTagName("select")[0].style.backgroundColor = null;
+//     document.getElementsByTagName("select")[0].style.color = null;
+//   });
 
-document.getElementsByTagName("nav")[0].addEventListener("mouseout", function () {
-  document.getElementsByTagName("nav")[0].style.backgroundColor = null;
-  document.getElementById("navHeading").style.color = null;
-  document.getElementsByTagName("ul")[0].style.color = null;
-});
+const currentYear = new Date().getFullYear();
+const copyrightSymbol = "All rights reserved  &copy; ";
+
+document.getElementsByTagName("p")[0].innerHTML =copyrightSymbol+currentYear ;
