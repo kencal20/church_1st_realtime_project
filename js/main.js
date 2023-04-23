a80057a38362d3885d3564108fbff34ccbd98ecc
kencal <kenadjei20@gmail.com>
2023-04-23 09:14:08 +0000

    updating the id's  calls from id to tagName

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-document.getElementById("nav").addEventListener("mouseover", function () {
-  document.getElementById("nav").style.backgroundColor = "white";
+document.getElementsByTagName("nav")[0].addEventListener("mouseover", function () {
+  document.getElementsByTagName("nav")[0].style.backgroundColor = "white";
   document.getElementById("navHeading").style.color = "black";
   document.getElementsByTagName("ul")[0].style.color = "black";
 
 });
 
-document.getElementById("nav").addEventListener("mouseout", function () {
-  document.getElementById("nav").style.backgroundColor = null;
+document.getElementsByTagName("nav")[0].addEventListener("mouseout", function () {
+  document.getElementsByTagName("nav")[0].style.backgroundColor = null;
   document.getElementById("navHeading").style.color = null;
   document.getElementsByTagName("ul")[0].style.color = null;
 });
